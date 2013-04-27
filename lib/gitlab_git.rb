57c7a5343db1fb46edcae4b65f2c39c5455edd78
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-04-27 12:33:28 +0300

    Extract code from gitlabhq

---
+++ lib/gitlab_git.rb
@@ -0,0 +1,13 @@
+require 'ostruct'
+require 'fileutils'
+require 'grit'
+require 'linguist'
+require "gitlab_git/popen"
+require "gitlab_git/blame"
+require "gitlab_git/blob"
+require "gitlab_git/commit"
+require "gitlab_git/compare"
+require "gitlab_git/diff"
+require "gitlab_git/repository"
+require "gitlab_git/stats"
+require "gitlab_git/tree"
---
+++ lib/gitlab_git/tree.rb
@@ -0,0 +1,52 @@
+module Gitlab
+  module Git
+    class Tree
+      attr_accessor :repository, :sha, :path, :ref, :raw_tree, :id
+
+      def initialize(repository, sha, ref = nil, path = nil)
+        @repository, @sha, @ref, @path = repository, sha, ref, path
+
+        @path = nil if !@path || @path == ''
+
+        # Load tree from repository
+        @commit = @repository.commit(@sha)
+        @raw_tree = @repository.tree(@commit, @path)
+      end
+
+      def exists?
+        raw_tree
+      end
+
+      def empty?
+        !data || data == ''
+      end
+
+      def trees
+        entries.select { |t| t.is_a?(Grit::Tree) }
+      end
+
+      def blobs
+        entries.select { |t| t.is_a?(Grit::Blob) }
+      end
+
+      def is_blob?
+        raw_tree.is_a?(Grit::Blob)
+      end
+
+      def up_dir?
+        path && path != ''
+      end
+
+      def readme
+        @readme ||= blobs.find { |c| c.name =~ /^readme/i }
+      end
+
+      protected
+
+      def entries
+        raw_tree.contents
+      end
+    end
+  end
+end
+
