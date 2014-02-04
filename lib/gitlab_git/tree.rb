9b6a224e5c298685de126c0bfc3fb96a298ea7fd
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-02-04 10:23:06 +0200

    Tree via rugged
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- lib/gitlab_git/tree.rb
+++ lib/gitlab_git/tree.rb
@@ -1,35 +1,61 @@
 module Gitlab
   module Git
     class Tree
-      attr_accessor :id, :name, :path, :type, :mode, :commit_id, :submodule_url
+      attr_accessor :id, :root_id, :name, :path, :type,
+        :mode, :commit_id, :submodule_url
 
       class << self
-        def where(repository, sha, path = '/')
-          commit = Commit.find(repository, sha)
-          grit_tree = commit.tree / path
+        def where(repository, sha, path = nil)
+          path = nil if path == '' || path == '/'
 
-          if grit_tree && grit_tree.respond_to?(:contents)
-            grit_tree.contents.map do |entry|
-              type = entry.class.to_s.split("::").last.downcase.to_sym
+          commit = repository.rugged.lookup(sha)
+          root_tree = commit.tree
 
-              Tree.new(
-                id: entry.id,
-                name: entry.name,
-                type: type,
-                mode: entry.mode,
-                path: path == '/' ? entry.name : File.join(path, entry.name),
-                commit_id: sha,
-                submodule_url: (type == :submodule) ? entry.url(sha) : nil
-              )
-            end
+          tree = if path
+                   id = Tree.find_id_by_path(root_tree, path)
+                   if id
+                     repository.rugged.lookup(id)
+                   else
+                     []
+                   end
+                 else
+                   root_tree
+                 end
+
+          tree.map do |entry|
+            Tree.new(
+              id: entry[:oid],
+              root_id: root_tree.oid,
+              name: entry[:name],
+              type: entry[:type] || :submodule,
+              mode: entry[:filemode],
+              path: path ? File.join(path, entry[:name]) : entry[:name],
+              commit_id: sha,
+            )
+          end
+        end
+
+        def find_id_by_path(root_tree, path)
+          path_arr = path.split('/')
+
+          entry = root_tree.find do |entry|
+            entry[:name] == path_arr[0] && entry[:type] == :tree
+          end
+
+          return nil unless entry
+
+          if path_arr.size > 1
+            path_arr.shift
+            tree = repository.rugged.lookup(entry[:oid])
+            find_id_by_path(tree, path_arr.join('/'))
           else
-            []
+            entry[:oid]
           end
         end
       end
 
       def initialize(options)
-        %w(id name path type mode commit_id submodule_url).each do |key|
+        %w(id root_id name path type mode commit_id).each do |key|
           self.send("#{key}=", options[key.to_sym])
         end
       end
