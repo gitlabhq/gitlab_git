527e3823a43226909daa83b7cd716b209d10cb42
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2014-02-27 15:11:11 +0200

    Fix compare specs
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -2,18 +2,16 @@ require "spec_helper"
 
 describe Gitlab::Git::Compare do
   let(:repository) { Gitlab::Git::Repository.new(TEST_REPO_PATH) }
-  let(:compare) { Gitlab::Git::Compare.new(repository,
-                                           "3a4b4fb4cde7809f033822a171b9feae19d41fff",
-                                           "8470d70da67355c9c009e4401746b1d5410af2e3") }
+  let(:compare) { Gitlab::Git::Compare.new(repository, SeedRepo::BigCommit::ID, SeedRepo::Commit::ID) }
 
   describe :commits do
     subject do
       compare.commits.map(&:id)
     end
 
-    it { should have(3).elements }
-    it { should include("f0f14c8eaba69ebddd766498a9d0b0e79becd633") }
-    it { should_not include("bcf03b5de6c33f3869ef70d68cf06e679d1d7f9a") }
+    it { should have(8).elements }
+    it { should include(SeedRepo::Commit::PARENT_ID) }
+    it { should_not include(SeedRepo::BigCommit::PARENT_ID) }
   end
 
   describe :diffs do
@@ -21,9 +19,9 @@ describe Gitlab::Git::Compare do
       compare.diffs.map(&:new_path)
     end
 
-    it { should have(19).elements }
-    it { should include('app/assets/javascripts/application.js') }
-    it { should_not include('Gemfile') }
+    it { should have(10).elements }
+    it { should include('files/ruby/popen.rb') }
+    it { should_not include('LICENSE') }
     it { compare.timeout.should be_false }
     it { compare.empty_diff?.should be_false }
   end
