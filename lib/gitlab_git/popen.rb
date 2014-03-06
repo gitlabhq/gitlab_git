f038ea90cbe3ae6c030c6e303bf7cfde94fde346
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-03-06 16:27:20 +0100

    Read output before waiting for process to end
    Sometimes a subprocess will not exit until it was able to write its
    output to stdout/stderr. By waiting _after_ we read the subprocess
    output, we avoid this potential bug.

--- lib/gitlab_git/popen.rb
+++ lib/gitlab_git/popen.rb
@@ -10,9 +10,9 @@ module Gitlab
         @cmd_output = ""
         @cmd_status = 0
         Open3.popen3(vars, cmd, options) do |stdin, stdout, stderr, wait_thr|
-          @cmd_status = wait_thr.value.exitstatus
           @cmd_output << stdout.read
           @cmd_output << stderr.read
+          @cmd_status = wait_thr.value.exitstatus
         end
 
         return @cmd_output, @cmd_status
