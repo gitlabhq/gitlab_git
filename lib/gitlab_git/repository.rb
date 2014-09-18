5cc4065b0b7ba437be7a1a4ddada21688f1a1d1e
Vinnie Okada <vokada@mrvinn.com>
2014-09-18 18:22:46 -0500

    Change #heads method
    Return Gitlab::Git::Ref objects instead of raw references.  Also stop
    caching `@heads` to avoid being out of sync with the repo.

--- lib/gitlab_git/repository.rb
+++ lib/gitlab_git/repository.rb
@@ -91,7 +91,9 @@ module Gitlab
 
       # Deprecated. Will be removed in 5.2
       def heads
-        @heads ||= grit.heads.sort_by(&:name)
+        rugged.references.each("refs/heads/*").map do |head|
+          Gitlab::Git::Ref.new(head.name, head.target)
+        end.sort_by(&:name)
       end
 
       def has_commits?
@@ -571,7 +573,6 @@ module Gitlab
       def checkout(ref, options = {}, start_point = "HEAD")
         if options[:b]
           rugged.branches.create(ref, start_point)
-          @heads = nil
           options.delete(:b)
         end
         default_options = { strategy: :safe_create }
@@ -581,7 +582,6 @@ module Gitlab
       # Delete the specified branch from the repository
       def delete_branch(branch_name)
         rugged.branches.delete(branch_name)
-        @heads = nil
       end
 
       # Return an array of this repository's remote names
