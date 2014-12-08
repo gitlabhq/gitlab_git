60651cdca67d74acbb45a1a19676743d9fd951b0
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-12-08 18:46:25 +0100

    Make version 6.0 work with the current test repo

--- spec/support/seed_helper.rb
+++ spec/support/seed_helper.rb
@@ -8,6 +8,13 @@ module SeedHelper
   def create_seeds
     puts 'Prepare seeds'
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(*%W(git clone --bare https://github.com/gitlabhq/testme.git), chdir: SUPPORT_PATH)
+    system(*%W(git clone --bare https://gitlab.com/gitlab-org/gitlab-git-test.git #{TEST_REPO_PATH}), chdir: SUPPORT_PATH)
+    File.open(File.join(TEST_REPO_PATH, 'refs/heads/master'), 'w') { |f| f.puts SeedRepo::LastCommit::ID }
+    IO.popen(%W(git -C #{TEST_REPO_PATH} tag)).read.each_line do |tag|
+      tag.chomp!
+      unless SeedRepo::Repo::TAGS.include?(tag)
+        system(*%W(git -C #{TEST_REPO_PATH} tag -d -- #{tag}))
+      end
+    end
   end
 end
