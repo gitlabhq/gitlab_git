203da3ad66d74e6cfab22d78946c3791bbef0ba1
Robert Speicher <rspeicher@gmail.com>
2015-04-01 22:07:14 -0400

    Define GIT_TEMPLATE_DIR environment variable in seed helper
    See https://github.com/gitlabhq/gitlabhq/pull/9044

--- spec/support/seed_helper.rb
+++ spec/support/seed_helper.rb
@@ -12,22 +12,28 @@ module SeedHelper
   def create_bare_seeds
     puts "Prepare seeds"
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(*%W(git clone --bare #{GITHUB_URL}), chdir: SUPPORT_PATH)
+    system(git_env, *%W(git clone --bare #{GITHUB_URL}), chdir: SUPPORT_PATH)
   end
 
   def create_normal_seeds
     puts "Prepare seeds"
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(*%W(git clone #{TEST_REPO_PATH} #{TEST_NORMAL_REPO_PATH}))
+    system(git_env, *%W(git clone #{TEST_REPO_PATH} #{TEST_NORMAL_REPO_PATH}))
   end
 
   def create_mutable_seeds
     puts 'Prepare seeds'
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(*%W(git clone #{TEST_REPO_PATH} #{TEST_MUTABLE_REPO_PATH}))
-    system(*%w(git branch -t feature origin/feature),
+    system(git_env, *%W(git clone #{TEST_REPO_PATH} #{TEST_MUTABLE_REPO_PATH}))
+    system(git_env, *%w(git branch -t feature origin/feature),
            chdir: TEST_MUTABLE_REPO_PATH)
-    system(*%W(git remote add expendable #{GITHUB_URL}),
+    system(git_env, *%W(git remote add expendable #{GITHUB_URL}),
            chdir: TEST_MUTABLE_REPO_PATH)
+  end
+
+  # Prevent developer git configurations from being persisted to test
+  # repositories
+  def git_env
+    {'GIT_TEMPLATE_DIR' => ''}
   end
 end
