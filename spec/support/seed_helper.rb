a13d81ae944955e609739bcdc7b177be62ffa7fc
Robert Speicher <rspeicher@gmail.com>
2015-04-06 20:29:52 -0400

    Silence SeedHelper

--- spec/support/seed_helper.rb
+++ spec/support/seed_helper.rb
@@ -10,22 +10,19 @@ module SeedHelper
   end
 
   def create_bare_seeds
-    puts "Prepare seeds"
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(git_env, *%W(git clone --bare #{GITHUB_URL}), chdir: SUPPORT_PATH)
+    system(git_env, *%W(git clone -q --bare #{GITHUB_URL}), chdir: SUPPORT_PATH)
   end
 
   def create_normal_seeds
-    puts "Prepare seeds"
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(git_env, *%W(git clone #{TEST_REPO_PATH} #{TEST_NORMAL_REPO_PATH}))
+    system(git_env, *%W(git clone -q #{TEST_REPO_PATH} #{TEST_NORMAL_REPO_PATH}))
   end
 
   def create_mutable_seeds
-    puts 'Prepare seeds'
     FileUtils.mkdir_p(SUPPORT_PATH)
-    system(git_env, *%W(git clone #{TEST_REPO_PATH} #{TEST_MUTABLE_REPO_PATH}))
-    system(git_env, *%w(git branch -t feature origin/feature),
+    system(git_env, *%W(git clone -q #{TEST_REPO_PATH} #{TEST_MUTABLE_REPO_PATH}))
+    system(git_env, *%w(git branch -q -t feature origin/feature),
            chdir: TEST_MUTABLE_REPO_PATH)
     system(git_env, *%W(git remote add expendable #{GITHUB_URL}),
            chdir: TEST_MUTABLE_REPO_PATH)
