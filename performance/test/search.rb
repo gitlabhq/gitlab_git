ca8401bc5b049cad444c33374090d62329ed7854
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2016-04-15 17:45:26 +0200

    Project is hosted on GitLab.com
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- performance/test/search.rb
+++
@@ -1,7 +0,0 @@
-require 'gitlab_git'
-require 'memory_benchmark'
-
-memory_benchmark do
-  repo = Gitlab::Git::Repository.new(ARGV.first)
-  repo.search_files('baz', '5a90ed56c6270627fe92def4eeca1ef150fc2d4c')
-end
